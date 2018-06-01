e3e6c559fc161e112b0b4c7d96ab9281e5125efa
Ricardo <rroque98@gmail.com>
2018-06-01 15:57:56 -0700

    Added a way to store multiple snippets by converting them to objects and pushing them into a snippets array.

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-$(document).ready(function() {
-  $('.setData').on('click', function() {
-    let textFieldValue = $('.textField').val();
-    // console.log(textFieldValue);
-    $('debug').text(textFieldValue);
-    localStorage.setItem('myFormTextData', textFieldValue);
-    $('.textField').val('');
-  });
-  $('.getData').on('click', function() {
-  	let retrieveData = localStorage.getItem('myFormTextData');
-  	$('.debug').text(retrieveData);
-  });
-  // $('.textField').on('keyup', function() {
-  // 	let textFieldValue = $('.textField').val();
-  // });
+var snippets = [];
 
+$(document).ready(function() {
   $('.setSnippet').on('click', function() {
     let textAreaValue = $('.textArea').val();
+    let textFieldValue = $('.textField').val();
     // console.log(textFieldValue);
     $('debug').text(textAreaValue);
+    localStorage.setItem('myFormTextData', textFieldValue);
+    $('.textField').val('');
     localStorage.setItem('myFormSnippetData', textAreaValue);
     $('.textArea').val('');
+    var individualSnippet= {};
+    individualSnippet[textFieldValue] = textAreaValue;
+    snippets.push(individualSnippet);
   });
   $('.getSnippet').on('click', function() {
   	let retrieveSnippetData = localStorage.getItem('myFormSnippetData');
   	$('.debug').text(retrieveSnippetData);
+  	let retrieveData = localStorage.getItem('myFormTextData');
+  	$('.debug').text(retrieveData);
   });
 });
+
+  // $('.setData').on('click', function() {
+  //   let textFieldValue = $('.textField').val();
+  //   // console.log(textFieldValue);
+  //   $('debug').text(textFieldValue);
+  //   localStorage.setItem('myFormTextData', textFieldValue);
+  //   $('.textField').val('');
+  // });
+  // $('.getData').on('click', function() {
+  // 	let retrieveData = localStorage.getItem('myFormTextData');
+  // 	$('.debug').text(retrieveData);
+  // });
+  // $('.textField').on('keyup', function() {
+  // 	let textFieldValue = $('.textField').val();
+  // });
