0c58995d66604f9b1c50b5e230833ba48779d25d
Ricardo <rroque98@gmail.com>
2018-06-01 11:32:49 -0700

    Added a textarea to hold snippet and added placeholders. Updated README

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ This is a create update delete app.
 - [x] Create an input form with text input
 - [x] Add a click handler
 - [x] Created helper utilites to save to and retrieve from local storage
+
+- [x] Create a textarea. Make it handle code! (use a textarea instead of a text)
+- [x] have two fields, one is a short name, the other is the snippet
+- [ ] going to store the name and code inside localStorage
+- [ ] display all snippets
+- [ ] search for a snippet using the name
+<!-- example: <pre>JSON.stringify.... -->
--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-	$('.setData').on('click', function() {
-		let textFieldValue = $('.textField').val();
-		console.log(textFieldValue);
-		$('debug').text(textFieldValue);
-
-		localStorage.setItem('myFormTextData', textFieldValue);
-		$('.textField').val('');
-	});
-
-	$('.getData').on('click', function() {
-		let retrieveData = localStorage.getItem('myFormTextData');
-		$('.debug').text(retrieveData);
-	});
-
-	// $('.textField').on('keyup', function() {
-	// 	let textFieldValue = $('.textField').val();
-	// });
+  $('.setData').on('click', function() {
+    let textFieldValue = $('.textField').val();
+    // console.log(textFieldValue);
+    $('debug').text(textFieldValue);
+    localStorage.setItem('myFormTextData', textFieldValue);
+    $('.textField').val('');
+  });
+  $('.getData').on('click', function() {
+  	let retrieveData = localStorage.getItem('myFormTextData');
+  	$('.debug').text(retrieveData);
+  });
+  // $('.textField').on('keyup', function() {
+  // 	let textFieldValue = $('.textField').val();
+  // });
 });
